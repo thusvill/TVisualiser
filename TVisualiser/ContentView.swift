0c18bcb7ccfe7ccd4ef25e17e63d6c1b551bfe5b
Bios Thusvill <87165098+thusvill@users.noreply.github.com>
2026-09-04 07:56:13 +0530

    Initial Commit

---
+++ TVisualiser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TVisualiser
+//
+//  Created by mac on 2026-09-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TVisualiser/TVisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVisualiserApp.swift
+//  TVisualiser
+//
+//  Created by mac on 2026-09-04.
+//
+
+import SwiftUI
+
+@main
+struct TVisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
